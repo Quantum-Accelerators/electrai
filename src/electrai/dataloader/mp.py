a338892d51369207af8377b46065d61e6a6a8bae
Hananeh Oliaei <ho0950@della-vis1.princeton.edu>
2025-12-16 18:23:16 -0500

    added normalization by cell volume to the chgcar dataloader

--- src/electrai/dataloader/mp.py
+++ src/electrai/dataloader/mp.py
@@ -127,8 +127,8 @@ class RhoData(Dataset):
         label = self.read_data(self.data[idx][1])
 
         if self.rho_type == "chgcar":
-            data = data.data["total"] / np.prod(data.data["total"].shape)
-            label = label.data["total"] / np.prod(label.data["total"].shape)
+            data = data.data["total"] / data.structure.lattice.volume
+            label = label.data["total"] / label.structure.lattice.volume
         else:
             data = data.data["total"]
             label = label.data["total"]
