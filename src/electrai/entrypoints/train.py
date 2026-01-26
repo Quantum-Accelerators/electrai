6b89550483c96ede863db17cb3c36853cb490e4e
Hananeh Oliaei <ho0950@della-vis1.princeton.edu>
2026-01-26 13:44:35 -0500

    updated train script

--- src/electrai/entrypoints/train.py
+++ src/electrai/entrypoints/train.py
@@ -87,13 +87,18 @@ def train(args):
     # -----------------------------
     # Trainer
     # -----------------------------
+    local_world_size = int(
+        os.environ.get("LOCAL_WORLD_SIZE", torch.cuda.device_count())
+    )
+    world_size = int(os.environ.get("WORLD_SIZE", local_world_size))
+    num_nodes = max(1, world_size // local_world_size)
     trainer = Trainer(
         max_epochs=int(cfg.epochs),
         logger=wandb_logger,
         callbacks=[checkpoint_cb, lr_monitor],
         accelerator="gpu" if torch.cuda.is_available() else "cpu",
-        devices=int(os.environ.get("SLURM_GPUS_PER_NODE", "1")),
-        num_nodes=int(os.environ.get("SLURM_JOB_NUM_NODES", "1")),
+        devices="auto",
+        num_nodes=num_nodes,
         strategy="ddp",
         precision=cfg.model_precision,
         log_every_n_steps=1,
